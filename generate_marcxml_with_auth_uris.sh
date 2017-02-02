30fee135b0e6f8182afeb6032540b47219ca0875
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-02-02 14:09:54 -0800

    generate_marcxml_with_auth_uris.sh refactored into a bash function.
    - this script now defines a bash function that processes one MARC file
    - this allows it to be used in another script that loops over MARC files
    - this will allow using this function in multiple shell processes
    - the function can be called directly as follows:
    
        ```sh
        source ./ld4p_configure.sh
        source ./generate_marcxml_with_auth_uris.sh
        generate_marcxml_with_auth_uris ${MARC21_BINARY_FILE}
        ```

--- generate_marcxml_with_auth_uris.sh
+++ generate_marcxml_with_auth_uris.sh
@@ -1,6 +1,5 @@
+# vim: autoindent tabstop=4 shiftwidth=4 expandtab softtabstop=4 filetype=sh
 #!/bin/bash
-# process all files in the Marc dir and use Marc4J and SQL to look up authority keys
-# get the 92X URI and put it in the subfield 0 - jkg
 
 # Check dependencies
 if [ "$LD4P_MARC" == "" ]; then
@@ -8,24 +7,42 @@ if [ "$LD4P_MARC" == "" ]; then
     source ${SCRIPT_PATH}/ld4p_configure.sh
 fi
 
-# Find and convert any dumped marc record files; archive processed files.
-echo
-echo "Searching for MARC files: ${LD4P_MARC}"
-for MRC_FILE in `find ${LD4P_MARC} -type f | sort`
-do
+# Bash function to convert one MARC file to XML files for each record.
+# Depends on the environment variables defined in ld4p_configure.sh
+# Depends on installation of the ld4p-tracer-bullets java library.
+# Requires one input parameter - the path to a MARC21 binary file.
+generate_marcxml_with_auth_uris () {
+
+    MRC_FILE=$1
+
+    stamp=$(date --iso-8601)
     filename=$(basename ${MRC_FILE} .mrc)
-    LOG_FILE="${LD4P_LOGS}/${filename}_MarcToXML.log"
-    SUCCESS=0
+    LOG_FILE="${LD4P_LOGS}/${filename}_MarcToXML_${stamp}.log"
+
     echo
     echo "Converting MARC file:  ${MRC_FILE}"
     echo "Output MARC-XML files: ${LD4P_MARCXML}/*.xml"
-    java -cp ${LD4P_JAR} org.stanford.MarcToXML ${MRC_FILE} ${LD4P_MARCXML} 2> ${LOG_FILE}
+    echo "Logging conversion to: ${LOG_FILE}"
+
+    options="-i ${MRC_FILE} -o ${LD4P_MARCXML} -l ${LOG_FILE}"
+    [ -n "${LD4P_MARCXML_REPLACE}" ] && options="${options} -r"
+
+    # Process all records in the MRC_FILE using marc4j and SQL to
+    # look up authority keys and retrieve any URI values from
+    # 92X fields and put them in the subfield 0 so that the 
+    # LOC converter (for Bibframe v1) can use them correctly.
+    java -cp ${LD4P_JAR} org.stanford.MarcToXML ${options}
+
     SUCCESS=$?
     if [ ${SUCCESS} ]; then
+	echo "Completed conversion."
+	echo "Moving MARC file to archive: ${LD4P_DATA}/Archive/Marc/"
 	mv $MRC_FILE ${LD4P_DATA}/Archive/Marc/
     else
-	echo
 	echo "ERROR: Conversion failed for ${MRC_FILE}" | tee --append ${LD4P_LOGS}/errors
     fi
-done
+    echo
+
+    return $SUCCESS
+}
 
