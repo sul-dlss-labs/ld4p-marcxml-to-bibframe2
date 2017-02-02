5d6a37e726078032b46427b3689159b326856b1f
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-02-02 14:16:41 -0800

    Adapt laptop script to use generate_marcxml_with_auth_uris as a function

--- laptop_reset_and_run_marcxml_conversion.sh
+++ laptop_reset_and_run_marcxml_conversion.sh
@@ -1,8 +1,14 @@
+# vim: autoindent tabstop=4 shiftwidth=4 expandtab softtabstop=4 filetype=sh
 #!/bin/bash
+
+# To replace exisiting MARC-XML files, run this script with
+#LD4P_MARCXML_REPLACE=true
+
 
 export LD4P_SIRSI=/data/src/dlss/ld4l/ld4p_data
 export LD4P_RDF=/data/src/dlss/ld4l/ld4p_rdf
 source ./ld4p_configure.sh
+source ./generate_marcxml_with_auth_uris.sh
 
 export LD4P_CONFIGS=${LD4P_SIRSI}/configs
 
@@ -24,16 +30,23 @@ if [ ! -f ${LD4P_BIN}/ld4p_converter.jar ]; then
 fi
 
 echo
-echo "Cleanup archived data, xml and log files"
+echo "Cleanup archived data and log files"
 rm -f ${LD4P_DATA}/Archive/Marc/*.mrc
 rm -f ${LD4P_DATA}/log/*.log
-rm -f ${LD4P_MARCXML}/*.xml
 
 echo
 echo "Copying private fixture data"
 cp -vp ${LD4P_CONFIGS}/files/*.mrc ${LD4P_MARC}/
 
+
+# Find and convert any dumped marc record files; archive processed files.
 echo
-echo "Running MarcToXML converter"
-./generate_marcxml_with_auth_uris.sh
+echo "Searching for MARC files: ${LD4P_MARC}"
+for MRC_FILE in `find ${LD4P_MARC} -type f | sort`
+do
+    generate_marcxml_with_auth_uris $MRC_FILE
+
+
+    exit  # Just process one file while testing things out.
+done
 
