1339f01b7c11717f03332398886857438dcfb8fc
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-02-08 13:28:48 -0800

    ld4p_install_* scripts - fail if not configured, do not try to auto-load config

--- ld4p_install_loc_marc2bibframe.sh
+++ ld4p_install_loc_marc2bibframe.sh
@@ -1,15 +1,16 @@
 # vim: autoindent tabstop=4 shiftwidth=4 expandtab softtabstop=4 filetype=sh
 #!/bin/bash
 
-SCRIPT_PATH=$(dirname $0)
-
 # This install script depends on prior configuration
-if [ "$LD4P_BIN" == "" ]; then
-    source ${SCRIPT_PATH}/ld4p_configure.sh
+if [ "$LD4P_CONFIGS" == "" ]; then
+    echo "LD4P_CONFIGS is undefined."
+    echo "Please try again after 'source ld4p_configure.sh'"
+    return 1
 fi
 
 # The LOC converter is from https://github.com/lcnetdev/marc2bibframe.git
 # Confirm the LOC converter source package is available
+SCRIPT_PATH=$(dirname $0)
 export LOC_M2B_PATH="${SCRIPT_PATH}/loc_marc2bibframe"
 export LOC_M2B_XQUERY="${LOC_M2B_PATH}/xbin/saxon.xqy"
 if [ ! -f "${LOC_M2B_PATH}" ]; then
@@ -21,6 +22,6 @@ fi
 if [ ! -f "${LOC_M2B_XQUERY}" ]; then
    echo "ERROR: The LD4P scripts require an LOC converter source: ${LOC_M2B_XQUERY}" 1>&2
    echo "The LOC converter is from https://github.com/lcnetdev/marc2bibframe.git" 1>&2
-   exit 1
+   return 1
 fi
 
--- ld4p_install_shared_configs.sh
+++ ld4p_install_shared_configs.sh
@@ -1,15 +1,16 @@
 # vim: autoindent tabstop=4 shiftwidth=4 expandtab softtabstop=4 filetype=sh
 #!/bin/bash
 
-SCRIPT_PATH=$(dirname $0)
-
 # This install script depends on prior configuration
 if [ "$LD4P_CONFIGS" == "" ]; then
-    source ${SCRIPT_PATH}/ld4p_configure.sh
+    echo "LD4P_CONFIGS is undefined."
+    echo "Please try again after 'source ld4p_configure.sh'"
+    return 1
 fi
 
-if [ ! -d ${LD4P_CONFIGS} ]; then
+if [ ! -d "${LD4P_CONFIGS}" ]; then
     # THIS IS A SUL PRIVATE REPOSITORY FOR SYSTEM CONFIGURATION DATA
+    echo "Trying to clone private repository for SUL-DLSS shared_configs"
     git clone git@github.com:sul-dlss/shared_configs.git ${LD4P_CONFIGS}
     pushd  ${LD4P_CONFIGS} > /dev/null
     git checkout ld4p-tracer-bullets
