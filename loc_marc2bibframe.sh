da67da28f744745eabcc0f9a0c28b0db2f7412cb
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-02-08 16:08:23 -0800

    loc_marc2bibframe.sh - revise logic on create/replace/skip and associated log message

--- loc_marc2bibframe.sh
+++ loc_marc2bibframe.sh
@@ -18,42 +18,46 @@ export M2B_LOG_FILE="${LD4P_LOGS}/Marc2bibframe_${stamp}.log"
 echo "LOC converter logs to M2B_LOG_FILE: ${M2B_LOG_FILE}"
 
 loc_marc2bibframe () {
-    MRC_XML=$1
+    stamp=$(date --iso-8601=sec)
 
+    MRC_XML=$1
     filename=$(basename "${MRC_XML}" ".xml")
     MRC_RDF="${LD4P_MARCRDF}/${filename}.rdf"
 
-    stamp=$(date --iso-8601=sec)
+    if [ "${LD4P_MARCRDF_REPLACE}" != ""  -o  "${MRC_XML}" -nt "${MRC_RDF}" ]; then
+        # LD4P_MARCRDF_REPLACE can force replacement for any existing RDF
+        # "${MRC_XML}" -nt "${MRC_RDF}" is:
+        #   True if MRC_XML exists and MRC_RDF does not, or
+        #   True if MRC_XML has been changed more recently than MRC_RDF.
+        if [ -f "${MRC_RDF}" ]; then
+            msg="${stamp}  REPLACED  MARC-RDF file: ${MRC_RDF}"
+        else
+            msg="${stamp}  CREATED   MARC-RDF file: ${MRC_RDF}"
+        fi
 
-    run=false
-    if [ "${LD4P_MARCRDF_REPLACE}" != "" ]; then
-        # Replace any existing RDF
-        run=true
-    elsif [ "${MRC_XML}" -nt "${MRC_RDF}" ]; then
-        # True if MRC_XML exists and MRC_RDF does not, or
-        # True if MRC_XML has been changed more recently than MRC_RDF.
-        run=true
+        java -cp ${LD4P_JAR} \
+            net.sf.saxon.Query ${LOC_M2B_XQUERY} \
+                marcxmluri="file://${MRC_XML}" \
+                baseuri=${LD4P_BASEURI} \
+                serialization="rdfxml" \
+                1> ${MRC_RDF} \
+                2>> ${M2B_LOG_FILE}
+
+        SUCCESS=$?
+        if [ ${SUCCESS} ]; then
+            mv ${MRC_XML} ${LD4P_ARCHIVE_MARCXML}
+        else
+            msg="${stamp}  FAILURE   MARC-RDF file: ${MRC_RDF}"
+        fi
+
     else
-        echo "${stamp}  SKIPPED   MARC-RDF file: ${MRC_RDF}" >> ${M2B_LOG_FILE}
-        return 0
+        # The MRC_RDF file must exist and it must be newer than MRC_XML and
+        # there is no forced replacement, so this conversion can be skipped.
+        msg="${stamp}  SKIPPED   MARC-RDF file: ${MRC_RDF}"
+        SUCCESS=0 # skipping an existing record is OK
     fi
 
-    java -cp ${LD4P_JAR} \
-        net.sf.saxon.Query ${LOC_M2B_XQUERY} \
-            marcxmluri="file://${MRC_XML}" \
-            baseuri=${LD4P_BASEURI} \
-            serialization="rdfxml" \
-            1> ${MRC_RDF} \
-            2>> ${M2B_LOG_FILE}
-
-    SUCCESS=$?
-    if [ ${SUCCESS} ]; then
-        echo "${stamp}  CONVERTED MARC-RDF file: ${MRC_RDF}" >> ${M2B_LOG_FILE}
-        mv ${MRC_XML} ${LD4P_ARCHIVE_MARCXML}
-    else
-        echo "${stamp}  FAILURE   MARC-RDF file: ${MRC_RDF}" >> ${M2B_LOG_FILE}
-    fi
-
+    echo "${msg}" >> ${M2B_LOG_FILE}
     return ${SUCCESS}
 }
 
