3942f9b53c092d531103b66147f0218351e0cc3e
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-02-11 10:59:17 -0800

    run_marc_xml2rdf_conversion.sh - search for *.xml files only

--- run_marc_xml2rdf_conversion.sh
+++ run_marc_xml2rdf_conversion.sh
@@ -7,8 +7,8 @@
 source ./loc_marc2bibframe.sh
 
 echo
-echo "Searching for MARC-XML files: ${LD4P_MARCXML}"
-for XML_FILE in `find ${LD4P_MARCXML} -type f | sort`
+echo "Searching for MARC-XML files: ${LD4P_MARCXML}/*.xml"
+for XML_FILE in `find ${LD4P_MARCXML} -type f -name '*.xml' | sort`
 do
     loc_marc2bibframe ${XML_FILE}
 done
