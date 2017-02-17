ea0d3921869defe344b4c7d45c2ca937e58185a8
Naomi Dushay <ndushay@stanford.edu>
2017-02-17 14:28:22 -0800

    remove files irrelevant to the purpose of THIS repo

--- bin/restore_marc_bin_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Restoring private MARC files from shared configs files"
-rsync -a --update ${LD4P_CONFIGS}/files/ ${LD4P_MARC}/
-
-echo "Restoring missing MARC files from archives in ${LD4P_ARCHIVE_MARC}/*.mrc"
-rsync -a --update ${LD4P_ARCHIVE_MARC}/ ${LD4P_MARC}/
-
